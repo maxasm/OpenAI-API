7d50fa3848be5a769b071b996bd1cc49a3a756a4
Maxwell Mbugua <maxwell@maaax.pro>
2023-06-22 19:25:42 +0300

    Clean up!

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-// this are the import
 import (
+	"time"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -12,19 +12,27 @@ import (
 )
 
 func main() {
+	// The API endpoint
 	const URL = "https://api.openai.com/v1/chat/completions"
 
-	client := &http.Client{}
+	timeout,err_timeout := time.ParseDuration("10s")
+	if err_timeout != nil {
+		log.Fatalf("%s\n", err_timeout)
+	}
+
+	// Create a client with a timeout of 10m
+	client := &http.Client{Timeout:timeout}
+
 	req, err_req := http.NewRequest("POST", URL, nil)
 	if err_req != nil {
-		log.Fatalf("%s\n", err_req.Error())
+		log.Fatalf("%s\n", err_req)
 	}
 
 	// Add the required Headers
 	req.Header.Add("Authorization", "Bearer "+os.Getenv("OPENAI_API_KEY"))
 	req.Header.Add("Content-Type", "application/json")
 
-	// sample request
+	// Create the request
 	api_req := OPEN_AI_API_REQUEST{
 		Model: GPT3,
 		Messages: []OPEN_AI_API_MESSAGE{
@@ -33,36 +41,37 @@ func main() {
 		},
 	}
 
-	// encode request to json
+	// Encode request to json
 	json_str, err_json_str := json.Marshal(api_req)
 	if err_json_str != nil {
-		log.Fatalf("%s\n", err_json_str.Error())
+		log.Fatalf("%s\n", err_json_str)
 	}
 
-	// req.Body -> io.ReadCloser
+	// You need an io.ReadCloser
 	str_read := bytes.NewBuffer(json_str)
 	str_read_closer := io.NopCloser(str_read)
-
 	req.Body = str_read_closer
 
+	// Send the request
 	resp, err_resp := client.Do(req)
 	if err_resp != nil {
-		log.Fatalf("%s\n", err_resp.Error())
+		log.Fatalf("%s\n", err_resp)
 	}
 
-	// type of data is []byte
+	// Read the response as []byte
 	data, err_data := io.ReadAll(resp.Body)
 	if err_data != nil {
-		log.Fatalf("%s\n", err_data.Error())
+		log.Fatalf("%s\n", err_data)
 	}
 
-	// Unmarshal the json-data
+	// Use json.Unmarshal to convert the json-encoded string to an object 
 	resp_obj := &OPEN_AI_API_RESPONSE{}
 	err_json := json.Unmarshal(data,resp_obj)
 	if err_json != nil {
 		log.Fatalf("%s\n",err_json)
 	}	
-
+	
+	// Print out the response
 	msg := resp_obj.Choices[0].Message
 	fmt.Printf("%s\n", msg.Content)	
 }
